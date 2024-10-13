55ffc45f0a040070491f4774890ca46855976c03
unknown <artemijtulchinskij@gmail.com>
2024-10-13 20:45:20 -0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+URL_SERVICE = "https://9a7ca345-1766-4aeb-b27b-a6f7ff378091.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
